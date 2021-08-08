2c1b6442badcdaa151723ea757ab316ca412275c
DeeChael <DeeChael@outlook.com>
2021-08-08 18:49:29 +0800

    Make all npc types based on zombie can set data to boolean to set if the npc is baby age

--- src/org/ezapi/module/npc/fake/FakeDrowned.java
+++ src/org/ezapi/module/npc/fake/FakeDrowned.java
@@ -16,6 +16,11 @@ public final class FakeDrowned extends FakeLiving {
 
     @Override
     public void data(Object entity, Object data) {
+        if (data instanceof Boolean) {
+            EzClass EntityZombie = new EzClass(Ref.getNmsOrOld("world.entity.monster.EntityZombie", "EntityZombie"));
+            EntityZombie.setInstance(entity);
+            EntityZombie.invokeMethod("setBaby", new Class[] {boolean.class}, new Object[] {data});
+        }
     }
 
 }
