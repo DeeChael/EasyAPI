ea9192a7095bd685ea01adc478bbd1263da48714
DeeChael <DeeChael@outlook.com>
2021-08-08 19:05:09 +0800

    Hoglin and Zoglin can set baby now

--- src/org/ezapi/module/npc/fake/FakeZoglin.java
+++ src/org/ezapi/module/npc/fake/FakeZoglin.java
@@ -16,6 +16,11 @@ public final class FakeZoglin extends FakeLiving {
 
     @Override
     public void data(Object entity, Object data) {
+        if (data instanceof Boolean) {
+            EzClass EntityZombie = new EzClass(Ref.getNmsOrOld("world.entity.monster.EntityZoglin", "EntityZoglin"));
+            EntityZombie.setInstance(entity);
+            EntityZombie.invokeMethod("setBaby", new Class[] {boolean.class}, new Object[] {data});
+        }
     }
 
 }
