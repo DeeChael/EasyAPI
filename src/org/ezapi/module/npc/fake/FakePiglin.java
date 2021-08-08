7d563861dbe1f9e53a1ad3ebd9848701a661640c
DeeChael <DeeChael@outlook.com>
2021-08-08 19:03:04 +0800

    Piglin can set baby now

--- src/org/ezapi/module/npc/fake/FakePiglin.java
+++ src/org/ezapi/module/npc/fake/FakePiglin.java
@@ -16,6 +16,11 @@ public final class FakePiglin extends FakeLiving {
 
     @Override
     public void data(Object entity, Object data) {
+        if (data instanceof Boolean) {
+            EzClass EntityZombie = new EzClass(Ref.getNmsOrOld("world.entity.monster.piglin.EntityPiglin", "EntityPiglin"));
+            EntityZombie.setInstance(entity);
+            EntityZombie.invokeMethod("setBaby", new Class[] {boolean.class}, new Object[] {data});
+        }
     }
 
 }
